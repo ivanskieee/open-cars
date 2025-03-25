5c184fca1dcc7a8cd8318b64d3cbf4f51948c886
Ivan Brilata <brilataivan86@gmail.com>
2025-03-25 15:16:08 +0800

    [update] mvc

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem "jbuilder"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'fast_jsonapi'
+
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,56 @@
+module Api
+  module V1
+    class CarsController < ApplicationController
+      def index
+        cars = Car.all
+
+        render json: CarSerializer.new(cars, options).serialized_json
+      end
+
+      def show
+        car = Car.find_by(slug: params[:slug])
+
+        render json: CarSerializer.new(car).serialized_json
+      end
+
+      def create
+        car = Car.new(car_params)
+
+        if car.save
+          render json: CarSerializer.new (car).serialized_json
+        else
+          render json: { error: car.errors.messages }, 
+        end
+      end
+
+      def update
+        car = Car.find_by(slug: params[:slug])
+
+        if car.update(car_params)
+          render json: CarSerializer.new (car, options).serialized_json
+        else
+          render json: { error: car.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        car = Car.find_by(slug: params[:slug])
+
+        if car.destroy
+          head :no_content
+        else
+          render json: { error: car.errors.messages }, status 422 
+        end
+      end
+
+      private
+
+      def car_params
+        params.require(:car).permit(:name, :image_url)
+      end
+
+      def options
+        @options ||= { include: %i[reviews] }
+    end
+  end
+end
